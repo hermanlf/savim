81cbfa0910a85b57642af653276e61cf4cf5b166
hermanlf <hermanlf@slminimalvm.(none)>
2011-12-22 08:44:02 -0800

    Updated the setup.sh script.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,59 @@
 #!/bin/bash
 
+symlink="$HOME/.vimrc"
+vimuseonly="$HOME/.vim/vimuseonly"
+undo="$vimuseonly/undo"
+tmp="$vimuseonly/tmp"
+backup="$vimuseonly/backup"
+workdone=false
+
 echo ' '
-echo 'Creating .vimrc symlink in your home directory.'
-echo ' '
-ln -s ~/.vim/.vimrc ~/.vimrc
+
+if [[ -f "$symlink" && ! -L "$symlink" ]]; then
+    echo "$symlink is a file. Removing..."
+    rm $HOME/.vimrc
+fi
+
+if [ -h "$symlink" ]; then
+    echo "Symlink exists"
+
+	linklocation=$(readlink "$symlink")
+
+    if [ $linklocation != "$HOME/.vim/.vimrc" ]; then
+        echo "Symlink points to incorrect location. Removing..."
+        rm $HOME/.vimrc
+        workdone=true
+    fi
+else
+    echo "Creating symlink $HOME/.vimrc"
+    ln -s $HOME/.vim/.vimrc $HOME/.vimrc
+    workdone=true
+fi
+
+if [ ! -d "$undo" ]; then
+    echo "creating undo directory $undo"
+    mkdir -p "$undo"
+    workdone=true
+fi
+
+if [ ! -d "$tmp" ]; then
+    echo "creating tmp directory $tmp"
+    mkdir -p "$tmp"
+    workdone=true
+fi
+
+if [ ! -d "$backup" ]; then
+    echo "creating backup directory $backup"
+    mkdir -p "$backup"
+    workdone=true
+fi
+
+if [ workdone ]; then
+    echo 'All necessary links and directories exist.'
+    echo 'Nothing left to do. Setup Complete!'
+    echo ' '
+else
+    echo 'Needed symlinks and directories exist.'
+    echo 'Nothing left to do. Setup Complete!'
+    echo ' '
+fi
