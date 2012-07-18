9cb07bf2feffe25793b6c0ebba22859b9da26c2c
hermanlf <hermanlf@slinuxvm.(none)>
2012-07-17 17:39:50 -0700

    Added seperate directory settings for Windows 7 and Windows XP

--- setup.cmd
+++
@@ -1,5 +0,0 @@
-mklink %userprofile%\.vimrc %userprofile%\.vim\.vimrc
-mkdir %userprofile%\.vim\.vimbackup
-mkdir %userprofile%\.vim\.vimswap
-mkdir %userprofile%\.vim\.vimundo
-mkdir %userprofile%\.vim\.vimviews
